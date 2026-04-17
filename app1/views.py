e093153ba6f863dd3bff3e499fd4f06d43c46423
surenarendra8-pixel <surenarendra8@gmail.com>
2026-04-17 22:07:14 +0530

    Add QR code to home page

--- app1/views.py
+++ app1/views.py
@@ -6,9 +6,11 @@ from django.shortcuts import render, redirect
 from app1.models import Register
 from app1.forms import Register_form
 
-def qr_code_view(request):
-    url = "https://registration-form-z3vc.onrender.com" 
+def new_registration(request):
+    info = Register.objects.all()
+    form = Register_form()
 
+    url = "https://registration-form-z3vc.onrender.com"
     qr = qrcode.QRCode(version=1, box_size=10, border=4)
     qr.add_data(url)
     qr.make(fit=True)
@@ -16,8 +18,14 @@ def qr_code_view(request):
     buffer = io.BytesIO()
     img.save(buffer, format="PNG")
     img_str = base64.b64encode(buffer.getvalue()).decode()
-    print("QR IMAGE LENGTH:", len(img_str))
-    return render(request, 'qr_code.html', {'qr_image': img_str})
+
+    if request.method == 'POST':
+        form = Register_form(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home_page')
+
+    return render(request, 'home.html', {'info': info, 'form': form, 'qr_image': img_str})
 
 def new_registration(request):
     info = Register.objects.all()
